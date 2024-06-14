011daef423ad2e0fa14f908d3d87d15f214f62c0
Chathu <87117983+ChathubhaniNDM@users.noreply.github.com>
2024-06-14 12:01:36 +0530

    Add files via upload
    commit

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php 
+
+	$connection = mysqli_connect('localhost', 'root', '', 'camper_shield');
+
+	if (mysqli_connect_errno()) {
+		echo mysqli_connect_err();
+	} 
+
+
+ ?>
---
+++ literature_survay.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<div class="container text-center">
+  <div class="row">
+    <div class="col">
+      <p>Project Scope</p>
+      <h5>Literature Survay</h5>
+    </div>
+    <div class="col">
+      Column
+    </div>
+  </div>
+</div>
+</body>
+</html>
